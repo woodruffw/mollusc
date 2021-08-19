879456a658c50956e1e859ee7b6e0c1c27a8d0cd
William Woodruff <william@yossarian.net>
2021-08-18 21:25:56 -0400

    bitstream: remove the `Value` class
    See the lengthy comment in `AbbrevOp::Parse` for the reason.

--- llvm-bitstream/src/record.rs
+++ llvm-bitstream/src/record.rs
@@ -1,38 +1,7 @@
 //! Record parsing and handling functionality for `llvm-bitstream`.
 
-use std::convert::TryFrom;
-
-use crate::error::Error;
-
-/// A higher-level representation of the individual values potentially found
-/// in a `Record`. The members here correspond roughly to
-/// [`AbbrevOpEnc`](llvm_constants::AbbrevOpEnc).
-#[derive(Clone, Debug)]
-pub enum Value {
-    /// An unsigned value, which may have come from a fixed field or a VBR.
-    Unsigned(u64),
-    /// An array of values.
-    Array(Vec<Value>),
-    /// A single Char6-encoded character.
-    Char6(u8),
-    /// An array of 8-bit objects.
-    Blob(Vec<u8>),
-}
-
-impl TryFrom<Value> for u64 {
-    type Error = Error;
-
-    fn try_from(value: Value) -> Result<u64, Self::Error> {
-        match value {
-            Value::Unsigned(val) => Ok(val),
-            Value::Char6(val) => Ok(val.into()),
-            _ => Err(Error::BadValue(format!(
-                "non-scalar field can't be made into a single number: {:?}",
-                value
-            ))),
-        }
-    }
-}
+/// A convenience alias for the fields of a record.
+pub type Fields = Vec<u64>;
 
 /// Represents a single bitstream record.
 #[derive(Debug)]
@@ -45,25 +14,25 @@ pub struct Record {
     pub code: u64,
 
     /// The fields of this record.
-    pub values: Vec<Value>,
+    pub fields: Fields,
 }
 
 impl Record {
-    /// Creates a new `Record` from the given code and values.
-    pub fn from_unabbrev(code: u64, values: Vec<Value>) -> Self {
+    /// Creates a new `Record` from the given code and fields.
+    pub fn from_unabbrev(code: u64, fields: Fields) -> Self {
         Self {
             abbrev_id: None,
             code: code,
-            values: values,
+            fields: fields,
         }
     }
 
-    /// Creates a new `Record` from the given abbreviation ID, code, and values.
-    pub fn from_abbrev(abbrev_id: u64, code: u64, values: Vec<Value>) -> Self {
+    /// Creates a new `Record` from the given abbreviation ID, code, and fields.
+    pub fn from_abbrev(abbrev_id: u64, code: u64, fields: Fields) -> Self {
         Self {
             abbrev_id: Some(abbrev_id),
             code: code,
-            values: values,
+            fields: fields,
         }
     }
 }
