0f9c1d1f8a132939bfa05b798a4d01081ef621a8
William Woodruff <william@yossarian.net>
2022-12-11 21:18:14 -0500

    mapper: use Clap 4 for example

--- llvm-mapper/examples/unroll-bitstream.rs
+++ llvm-mapper/examples/unroll-bitstream.rs
@@ -2,12 +2,12 @@ use std::convert::TryFrom;
 use std::fs;
 
 use anyhow::Result;
-use clap::{App, Arg};
+use clap::{Arg, Command};
 use llvm_bitstream::Bitstream;
 use llvm_mapper::unroll::Bitcode;
 
-fn app<'a>() -> App<'a> {
-    App::new(env!("CARGO_PKG_NAME"))
+fn app() -> Command {
+    Command::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(
@@ -23,7 +23,7 @@ fn main() -> Result<()> {
     let matches = app().get_matches();
 
     let input = {
-        let input = matches.value_of("input").unwrap();
+        let input = matches.get_one::<String>("input").unwrap();
         fs::read(input)?
     };
 
